e236a2e6a5ff6be06ff44487528db3974d7ac5a3
levani132 <lberoshvili9@gmail.com>
2018-04-24 15:40:51 +0400

    test commit 1

--- AngularJsMVC/Scripts/Myapp.js
+++ AngularJsMVC/Scripts/Myapp.js
@@ -67,10 +67,10 @@ app.controller("Myapp", function myfunction($scope, $http) {
         $http.post("/Home/GetCategories").then(function (response) {
             if (response.data.success) {
                 $scope.categories = response.data
-                console.log();
+                console.log(response.data);
             }
             else {
-                console.log();
+                console.log(response.data);
             }
         })
     }
